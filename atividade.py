d8c76e9f61bbe5e04df04fd5a4e627bc29b497ea
AianaSenai <aiana.lima@ba.estudante.senai.br>
2024-09-25 17:54:20 +0000

    salvar

--- atividade.py
+++ atividade.py
@@ -1,4 +1,113 @@
 import os
 os.system("cls||clear")
 
-print ()
+valor_1 = 15
+valor_2 = 20
+
+print("= CARDÁPIO DO DIA =")
+print (""""
+    código = 1 - Morango
+    código = 2 - Chololate
+    código = 3 - Leite Ninho
+    código = 4- Abacaxi
+    código = 5- Melancia
+    código = 6 - Ninho trufado
+    código = 7- Chocolate alpino
+     """)
+
+prato = int(input("Digite o numero do prato escolhido: "))
+
+
+while True:
+    prato = "123"
+    print ("""
+    1- A Vista
+    2- Cartão de Credito""")
+    pagamento = int(input("Digite a forma de pagamento: "))
+    if pagamento == "1":
+        total = valor_1 * 0.10
+        print(f"Total: {total}")
+    elif pagamento == "2":
+        total = valor_1 + 0.10
+        print(f"Total: {total}")
+        break
+  
+            
+    prato= "244"
+    print ("""
+    1- A Vista
+    2- Cartão de Credito""")
+    pagamento = int(input("Digite a forma de pagamento: "))
+    if pagamento == "1":
+        total = valor_1 * 0.10
+        print(f"Total: {total}")
+    elif pagamento == "2":
+        total = valor_1 + 0.10
+        print(f"Total: {total}")
+        break
+  
+    
+
+    prato= 357
+    print ("""
+    1- A Vista
+    2- Cartão de Credito""")
+    pagamento = int(input("Digite a forma de pagamento: "))
+    if pagamento == 1:
+        total = valor_1 * 0.10
+        print(f"Total: {total}")
+    
+    elif pagamento == 2:
+        total = valor_1 + 0.10
+        print(f"Total: {total}")
+    break
+  
+    
+    
+    prato= "424"
+    print ("""
+    1- A Vista
+    2- Cartão de Credito""")
+    pagamento = int(input("Digite a forma de pagamento: "))
+    if pagamento == "1":
+        total = valor_1 * 0.10
+        print(f"Total: {total}")
+        break
+    elif pagamento == "2":
+        total = valor_1 + 0.10
+        print(f"Total: {total}")
+        break
+  
+    
+
+    prato= "556"
+    print ("""
+    1- A Vista
+    2- Cartão de Credito""")
+    pagamento = int(input("Digite a forma de pagamento: "))
+    if pagamento == "1":
+        total = valor_1 * 0.10
+        print(f"Total: {total}")
+        break
+    elif pagamento == "2":
+        total = valor_1 + 0.10
+        print(f"Total: {total}")
+        break
+  
+    prato= "656"
+    pagamento = float(input("Digite a forma de pagamento: "))
+    print ("""
+    1- A Vista
+    2- Cartão de Credito""")
+
+    prato= "656"
+    pagamento = float(input("Digite a forma de pagamento: "))
+    print ("""
+    1- A Vista
+    2- Cartão de Credito""")
+
+    prato = "0"
+    print("Codigo invalido")
+    print(input("Digite o codigo novamente: "))
+    break
+     
